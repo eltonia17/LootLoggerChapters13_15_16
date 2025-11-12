de6aaa4e30774560771ce631abd28a915e8ddcc0
Leonard <esl107@miami.edu>
2025-11-11 23:27:17 -0500

    Entire Ch 13 before bronze challenge

--- LootLogger/ItemStore.swift
+++ LootLogger/ItemStore.swift
@@ -10,7 +10,14 @@ import UIKit
 class ItemStore {
 
     var allItems = [Item]()
-    
+    let itemArchiveURL: URL = {
+        let documentsDirectories =
+            FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.appendingPathComponent("items.plist")
+    }()
+
+    //Adding an item creation method
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
 
@@ -39,8 +46,39 @@ class ItemStore {
         // Insert item in array at new location
         allItems.insert(movedItem, at: toIndex)
     }
+    @objc func saveChanges() -> Bool {
+        
+        print("Saving items to: \(itemArchiveURL)")
 
+        do {
+                let encoder = PropertyListEncoder()
+                let data = try encoder.encode(allItems)
+                try data.write(to: itemArchiveURL, options: [.atomic])
+                    print("Saved all of the items")
+                    return true
+            } catch let encodingError {
+                print("Error encoding allItems: \(encodingError)")
+                return false
+            }
+       
+    }
     
+    init() {
+        do {
+                let data = try Data(contentsOf: itemArchiveURL)
+                let unarchiver = PropertyListDecoder()
+                let items = try unarchiver.decode([Item].self, from: data)
+                allItems = items
+            } catch {
+                print("Error reading in saved items: \(error)")
+            }
+
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(saveChanges),
+                                       name: UIScene.didEnterBackgroundNotification,
+                                       object: nil)
+    }
     
 //    init() {
 //        for _ in 0..<5 {
